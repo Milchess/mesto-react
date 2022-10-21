6066e5671a61b837fbe49c69af69cf760cf6fac3
Milchess <milch.ks.2992@gmail.com>
2022-10-21 19:33:51 +0500

    портирован модуль Api, реализована загрузка данных пользователя

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,29 @@
 import React from "react";
+import api from "../utils/api";
 
 function Main(props) {
+    const [userName, setUserName] = React.useState('');
+    const [userDescription, SetUserDescription] = React.useState('');
+    const [userAvatar, SetUserAvatar] = React.useState('');
+
+    api.getUserInformation()
+        .then((UserData) => {
+            setUserName(UserData.name);
+            SetUserDescription(UserData.about);
+            SetUserAvatar(UserData.avatar);
+        })
+        .catch(err => console.log(`Ошибка.....: ${err}`))
+
     return (
         <div className="content">
             <section className="profile">
                 <button className="profile__avatar-button" onClick={props.onEditAvatar}>
-                    <img alt="аватар пользователя" className="profile__avatar" src="src/components/App#"/>
+                    <img alt="аватар пользователя" className="profile__avatar" src={userAvatar}/>
                 </button>
                 <div className="profile__info">
-                    <h1 className="profile__user-name">Ваше имя</h1>
+                    <h1 className="profile__user-name">{userName}</h1>
                     <button aria-label="Редактировать" className="profile__edit-button" id="edit-button" type="button" onClick={props.onEditProfile}></button>
-                    <p className="profile__user-vocation">Ваш род деятельности</p>
+                    <p className="profile__user-vocation">{userDescription}</p>
                 </div>
                 <button aria-label="Добавить" className="profile__add-button" id="add-button" type="button" onClick={props.onAddPlace}></button>
             </section>
---
+++ src/utils/api.js
@@ -0,0 +1,113 @@
+import React from "react";
+
+class Api extends React.Component {
+    constructor(props) {
+        super(props);
+
+            this._baseUrl = 'https://mesto.nomoreparties.co/v1/cohort-51/';
+            this._headers = {
+                authorization: '38f4a1b9-fdca-415d-86b5-0f7384ead109',
+                'Content-Type': 'application/json'
+            }
+        }
+
+    _get(link) {
+        return fetch(this._baseUrl + link, {
+            method: 'GET',
+            headers: this._headers,
+        })
+            .then((res) => this._getResponseData(res))
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    _post(link, model) {
+        return fetch(this._baseUrl + link, {
+            method: 'POST',
+            headers: this._headers,
+            body: JSON.stringify(model)
+        })
+            .then((res) => this._getResponseData(res))
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    _patch(link, model) {
+        return fetch(this._baseUrl + link, {
+            method: 'PATCH',
+            headers: this._headers,
+            body: JSON.stringify(model)
+        })
+            .then((res) => this._getResponseData(res))
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    _put(link) {
+        return fetch(this._baseUrl + link, {
+            method: 'PUT',
+            headers: this._headers,
+        })
+            .then((res) => this._getResponseData(res))
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    _delete(link) {
+        return fetch(this._baseUrl + link, {
+            method: 'DELETE',
+            headers: this._headers,
+        })
+            .then((res) => this._getResponseData(res))
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    _getResponseData(res) {
+        if (res.ok) {
+            return res.json();
+        }
+        return Promise.reject(`Ошибка: ${res.status}`)
+    }
+
+    getInitialCards() {
+        return this._get('cards');
+    }
+
+    getUserInformation() {
+        return this._get('users/me');
+    }
+
+    setUserUpdate(model) {
+        return this._patch('users/me', model);
+    }
+
+    setCreateCard(model) {
+        return this._post('cards', model);
+    }
+
+    setDeleteCard(cardId) {
+        return this._delete(`cards/${cardId}`);
+    }
+
+    setLikeCard(cardId) {
+        return this._put(`cards/${cardId}/likes`);
+    }
+
+    setDeleteLikeCard(cardId) {
+        return this._delete(`cards/${cardId}/likes`);
+    }
+
+    setUserAvatar(model) {
+        return this._patch('users/me/avatar', model);
+    }
+}
+
+const api = new Api();
+
+export default api;
